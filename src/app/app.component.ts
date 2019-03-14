792626c2462ad0db240374ef3bfa979669d52e94
Ubadah Atta <ubadahatta12@gmail.com>
2019-03-14 17:35:37 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
+//decorator 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //where the component will be loaded in the index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todolist';
+ 
 }
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  //private = it only works here
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    })
+  }
+
+  deleteTodo(todo: Todo) {
+    //Remove from UI
+    this.todos = this.todos.filter(t => t.title !== todo.title);
+    //Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo){
+  this.todoService.addTodo(todo).subscribe(todo=>{
+    this.todos.push(todo);
+  })
+  }
+
+}
